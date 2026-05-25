f8506000a43e5a896789052d55fd1f9c5bbf3198
perewarvaibhav-svg <perewarvaibhav@gmail.com>
2026-05-25 14:04:45 +0530

    feat: perfectly reconstruct the exact structural geometry of the reference logo as SVG, adapted to the brand colour palette

--- src/components/LandingPage/Footer/Footer.jsx
+++ src/components/LandingPage/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { C } from "@/constants/designTokens";
+import { C, GRAD } from "@/constants/designTokens";
 
 export const Footer = () => (
   <footer
@@ -21,22 +21,29 @@ export const Footer = () => (
     >
       {/* Logo */}
       <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
-        <div
-          style={{
-            width: 36,
-            height: 36,
-            background: GRAD.primary,
-            WebkitMaskImage: 'url(/logo.png)',
-            WebkitMaskSize: 'contain',
-            WebkitMaskRepeat: 'no-repeat',
-            WebkitMaskPosition: 'center',
-            maskImage: 'url(/logo.png)',
-            maskSize: 'contain',
-            maskRepeat: 'no-repeat',
-            maskPosition: 'center',
-            flexShrink: 0,
-          }}
-        />
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 110 110"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ flexShrink: 0 }}
+        >
+          <defs>
+            <linearGradient id="gBrandGradFooter" x1="0%" y1="0%" x2="100%" y2="100%">
+              <stop offset="0%" stopColor="#ff4d66" />
+              <stop offset="30%" stopColor="#ff1938" />
+              <stop offset="100%" stopColor="#b3001b" />
+            </linearGradient>
+          </defs>
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M 10 75 L 10 40 C 10 20 25 10 45 10 L 90 10 C 95 10 100 15 100 25 L 100 90 C 100 95 95 100 90 100 L 35 100 L 10 75 Z M 40 80 L 40 70 L 75 70 L 75 50 L 40 50 L 40 35 C 40 32 42 30 45 30 L 80 30 L 80 80 L 40 80 Z"
+            fill="url(#gBrandGradFooter)"
+          />
+          <circle cx="60" cy="40" r="7" fill="url(#gBrandGradFooter)" />
+        </svg>
         <span
           style={{
             color: C.textPrimary,
--- src/components/LandingPage/Navbar.jsx
+++ src/components/LandingPage/Navbar.jsx
@@ -40,22 +40,29 @@ export const Navbar = () => {
       >
         {/* Logo */}
         <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
-          <div
-            style={{
-              width: 44,
-              height: 44,
-              background: GRAD.primary,
-              WebkitMaskImage: 'url(/logo.png)',
-              WebkitMaskSize: 'contain',
-              WebkitMaskRepeat: 'no-repeat',
-              WebkitMaskPosition: 'center',
-              maskImage: 'url(/logo.png)',
-              maskSize: 'contain',
-              maskRepeat: 'no-repeat',
-              maskPosition: 'center',
-              flexShrink: 0,
-            }}
-          />
+          <svg
+            width="38"
+            height="38"
+            viewBox="0 0 110 110"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ flexShrink: 0 }}
+          >
+            <defs>
+              <linearGradient id="gBrandGradNav" x1="0%" y1="0%" x2="100%" y2="100%">
+                <stop offset="0%" stopColor="#ff4d66" />
+                <stop offset="30%" stopColor="#ff1938" />
+                <stop offset="100%" stopColor="#b3001b" />
+              </linearGradient>
+            </defs>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M 10 75 L 10 40 C 10 20 25 10 45 10 L 90 10 C 95 10 100 15 100 25 L 100 90 C 100 95 95 100 90 100 L 35 100 L 10 75 Z M 40 80 L 40 70 L 75 70 L 75 50 L 40 50 L 40 35 C 40 32 42 30 45 30 L 80 30 L 80 80 L 40 80 Z"
+              fill="url(#gBrandGradNav)"
+            />
+            <circle cx="60" cy="40" r="7" fill="url(#gBrandGradNav)" />
+          </svg>
           <span
             style={{
               color: C.textPrimary,
